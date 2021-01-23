6cdf11fe77b0e8732f9f6158e9af410dacd1713e
Russell Schmidt <russell.schmidt@me.com>
2021-01-23 14:08:49 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import { Auth } from 'aws-amplify';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => Auth.federatedSignIn({
+          provider: "Facebook"
+        })}>Sign in with Facebook</button>
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
       </header>
     </div>
   );
